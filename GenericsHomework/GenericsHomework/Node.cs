806b79f0da8ed2a5e14f068a7cb5080097856b3c
ccornell88 <129800566+ccornell88@users.noreply.github.com>
2024-10-31 09:03:06 -0700

    Added Append and Exists Methods

--- GenericsHomework/GenericsHomework/Node.cs
+++ GenericsHomework/GenericsHomework/Node.cs
@@ -1,22 +1,52 @@
 ﻿
-namespace CircularLinkedList
+namespace GenericsHomework;
 
+public class Node<T>
 {
-    public class Node<T>
+    public T Value { get; }
+    public Node<T> Next { get; private set; }
+
+    public Node(T value)
     {
-        public T Value { get; }
-        public Node<T> Next { get; private set; }
-
-
-        public Node(T value)
-        {
-            Value = value;
-            Next = this;
-        }
-
-        public override string? ToString()
-        {
-            return Value?.ToString();
-        }
+        Value = value;
+        Next = this;
     }
+
+    public void Append(T newValue)
+    {
+        if (Exists(newValue))
+        {
+            throw new ArgumentException("No Duplicates");
+        }
+
+        Node<T> newNode = new(newValue)
+        {
+            Next = this.Next
+        };
+        this.Next = newNode;
+    }
+
+    public bool Exists(T newValue)
+    {
+        Node<T> current = this;
+
+        do
+        {
+            if (current.Value!.Equals(newValue))
+            {
+                return true;
+            }
+            current = current.Next;
+        }
+        while (current != this);
+
+        return false;
+    }
+
+    public override string? ToString()
+    {
+        return Value?.ToString();
+    }
+
 }
+
